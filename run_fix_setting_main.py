458b3ab9ba9cbcc64c15e82755c6ced8fd7a2123
SolenoidWGT <877825076@qq.com>
2024-01-12 06:36:12 +0000

    fix

--- run_fix_setting_main.py
+++ run_fix_setting_main.py
@@ -6,30 +6,13 @@ import pandas as pd
 from simulator.noz3_simulator import Constraint
 from utils.config import Config
 
-
 if __name__ == "__main__":
     GLOBA_BSZ = 512 * 1024
-    model_sizes = [7, 13, 30, 65]
+    model_sizes = [7]
     results = []
     columns = []
+    world_size = 1024
     for model_size in model_sizes:
-        if model_size == 7:
-            sp = 4
-            pp = 1
-            world_size = 128
-        elif model_size == 13:
-            sp = 8
-            pp = 1
-            world_size = 256
-        elif model_size == 30:
-            sp = 8
-            pp = 2
-            world_size = 512
-        elif model_size == 65:
-            sp = 8
-            pp = 4
-            world_size = 1024
-
         config = Config(
             {
                 "world_size_max": world_size,
@@ -37,7 +20,7 @@ if __name__ == "__main__":
                 "global_bsz": GLOBA_BSZ,
                 "global_bsz_min": GLOBA_BSZ,
                 "global_bsz_max": GLOBA_BSZ,
-                "sequence_length": 256 * 1024,
+                "sequence_length": 2 * 1024,
                 "model_size": model_size,
                 "vocab_size": 103168,
                 "dtype_size": 2,
@@ -60,51 +43,57 @@ if __name__ == "__main__":
         for algo_type in externl_sim._algo_list:
             if algo_type == "isp":
                 algo_name = "DSP"
-                wp = world_size // pp
+                pp = 1
+                sp = 1
+                wp = 1
+                zp = 128
             elif algo_type == "fsp":
                 algo_name = "Megatron-3D"
+                pp = 1
+                sp = 2
                 wp = 1
+                zp = 64
             elif algo_type == "msp":
                 algo_name = "Megatron-sp"
+                pp = 1
+                sp = 2
                 wp = 1
-                continue
+                zp = 64
 
             name = f"{model_size}B-{algo_name}"
             solu = externl_sim.cal_cost(
                 pp=pp,
                 sp=sp,
                 wp=wp,
-                zp=1,
-                micro_bsz=1,
+                zp=zp,
+                micro_bsz=4,
                 micro_num=1,
                 algo_type=algo_type,
                 world_size=world_size,
                 activation_ckpt=False,
-
             )
+            assert solu is not None
             if solu is None:
                 print(f"Warning model_size: {model_size}, algo_name: {algo_name}  Not found solu!", flush=True)
-                results.append(
-                    [
-                        name,
-                        0,0,0,0
-                    ]
-                )
+                results.append([name, 0, 0, 0, 0, 0])
             else:
                 print(f"model_size: {model_size}, algo_name: {algo_name}  found solu:{solu}!", flush=True)
                 comp = solu.comp_attn + solu.comp_wp
-                comm = solu.wp_comm_cost + solu.sp_comm_cost + solu.pp_comm_cost + solu.wdp_comm_cost + solu.zp_comm_cost
+                comm = (
+                    solu.wp_comm_cost + solu.sp_comm_cost + solu.pp_comm_cost + solu.wdp_comm_cost + solu.zp_comm_cost
+                )
                 results.append(
                     [
                         name,
-                        round(solu.activation / 1024**3, 2),
-                        round(solu.p_g_mm_cost / 1024**3, 2),
-                        round(comp, 2),
-                        round(comm, 2),
+                        round(solu.activation / 1024**3, 5),
+                        round(solu.p_g_mm_cost / 1024**3, 5),
+                        round(solu.os_mm_cost / 1024**3, 5),
+                        round(comp, 5),
+                        round(comm, 5),
                     ]
                 )
 
-    columns = ["algo", "act mem(GB)", "p mem(GB)", "comp(s)", "comm(s)"]
+    columns = ["algo", "act mem(GB)", "p mem(GB)", "os mem(GB)", "comp(s)", "comm(s)"]
     df = pd.DataFrame(results, columns=columns)
     print(df)
     df.to_csv("excel_output.xls")
